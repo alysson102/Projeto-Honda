bda591fdd84ac0049754f3c32098b672b8af0a6d
aliso1souzw-star <aliso.1souzw@gmail.com>
2026-04-01 11:29:29 -0300

    ...

--- app/Views/home/contact.php
+++ app/Views/home/contact.php
@@ -7,10 +7,10 @@
     <div class="info-grid info-grid-single">
         <article class="info-card">
             <h2>Atendimento</h2>
-            <p><strong>Telefone:</strong> (11) 4000-1000</p>
-            <p><strong>E-mail:</strong> contato@atlanticamotos.com.br</p>
-            <p><strong>Horario:</strong> Segunda a sexta, das 9h as 18h</p>
-            <p><strong>Endereco:</strong> Av. Exemplo, 1000 - Sao Paulo/SP</p>
+            <p><strong>Telefone:</strong> (82) 987229890</p>
+            <p><strong>E-mail:</strong> pecasportocalvo@atlanticamotos.com.br</p>
+            <p><strong>Horario:</strong> Segunda a sexta, das 7h às 13h, das 15h às 17h; sábado, das 7h às 11h.</p>
+            <p><strong>Endereco:</strong> Rodovia AL-101 norte, s/n - Centro - Porto Calvo/AL</p>
         </article>
     </div>
 </section>
