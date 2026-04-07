d53bb6e27d72cab5a8b9633a8fb495ca1a65e7d8
aliso1souzw-star <aliso.1souzw@gmail.com>
2026-04-07 10:53:33 -0300

    ...

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -17,6 +17,15 @@
 			</div>
 		</div>
 	</a>
+
+	<a class="card-link" href="<?= e(url('/pecas')) ?>">
+		<div class="card">
+			<img src="<?= e(url('/assets/imagens/info-revisao.png')) ?>">
+			<div class="card-content">
+				<h3 class="card-title-label card-title-label--revisoes">Informações sobre as revisões</h3>
+			</div>
+		</div>
+	</a>
 </section>
 
 
@@ -26,7 +35,7 @@
   <div class="carousel" id="carousel">
 		<div class="item"><a href="<?= e(url('/agendamento')) ?>" data-mobile-redirect="agendamento"><img src="<?= e(url('/assets/imagens/agen-retrato.png')) ?>"><span class="item-label">Agendamento</span></a></div>
 		<div class="item"><a href="<?= e(url('/pecas')) ?>" data-mobile-redirect="pecas"><img src="<?= e(url('/assets/imagens/pecas-retrato.png')) ?>"><span class="item-label">Peças Originais</span></a></div>
-    
+		<div class="item"><a href="<?= e(url('/pecas')) ?>" data-mobile-redirect="pecas"><img src="<?= e(url('/assets/imagens/info-revisao-retrato.jpeg')) ?>"><span class="item-label item-label--revisoes">Informações sobre as revisões</span></a></div>
 	
 	<div class="item active"></div>
     <div class="item"></div>
